bc3484999d53ab051ae6c99680e803b54d43035b
Vladislav <fcritic@users.noreply.github.com>
2025-04-07 20:47:44 +0300

    изменил роуты и конфиг интеграции

--- config/autoload/integrations.global.php
+++ config/autoload/integrations.global.php
@@ -3,25 +3,25 @@
 declare(strict_types=1);
 
 use App\Enum\IntegrationType;
-use Integration\Middleware\LoggerMiddleware;
+use Integration\Middleware\AmoJoLoggerMiddleware;
 
 return [
-    'type' => IntegrationType::TELEGRAM_AMOJO->value,
+    'type' => IntegrationType::TELEGRAM_AMOJO,
     'enabled' => true,
+    'host' => 'https://rich-novel-jackal.ngrok-free.app',
     'amocrm' => [
         'client_id' => $_ENV['CLIENT_ID'],
         'client_secret' => $_ENV['CLIENT_SECRET'],
         'redirect_uri' => $_ENV['REDIRECT_URI'],
-        'amojo' => [
-            'channel' => [
-                'uid' => $_ENV['CHANNEL_ID'],
-                'secret_key' => $_ENV['SECRET_KEY']
-            ],
-            'segment' => 'ru',
-            'middleware' => [
-                LoggerMiddleware::class,
-            ]
-        ],
+    ],
+    'amojo' => [
+        'channel_uid' => $_ENV['CHANNEL_ID'],
+        'secret_key' => $_ENV['SECRET_KEY'],
+        'channel_code' => $_ENV['CHANNEL_CODE'],
+        'segment' => 'ru',
+        'middleware' => [
+            AmoJoLoggerMiddleware::class,
+        ]
     ],
     'external_gateway' => [
         'telegram_url' => $_ENV['TELEGRAM_URL']
--- config/routes.php
+++ config/routes.php
@@ -2,24 +2,23 @@
 
 declare(strict_types=1);
 
-use AmoCRM\Handler\AmoJoWebhookHandler;
-use AmoCRM\Middleware\AmoJoWebhookMiddleware;
-use Integration\Handler\SettingsIntegrationHandler;
-use Integration\Middleware\SettingsIntegrationMiddleware;
 use Mezzio\Application;
 use Mezzio\MiddlewareFactory;
 use Psr\Container\ContainerInterface;
-use Telegram\Handler\TelegramWebhookHandler;
-use Telegram\Middleware\TelegramWebhookMiddleware;
 
 return static function (Application $app, MiddlewareFactory $factory, ContainerInterface $container): void {
-    $app->get('/api/oauth', \AmoCRM\Handler\OAuthAmoHandler::class);
+    $app->get('/api/amocrm/installing-integration', \AmoCRM\Handler\InstallingIntegrationHandler::class);
+    $app->post('/api/amocrm/installing-widget', [
+        \Integration\Middleware\InstallingWidgetMiddleware::class,
+        \Integration\Handler\InstallingWidgetHandler::class
+    ]);
+    $app->post('/api/amocrm/webhook/amojo/{scope_id}', [
+        \AmoCRM\Middleware\AmoJoWebhookMiddleware::class,
+        \AmoCRM\Handler\AmoJoWebhookHandler::class
+    ]);
     $app->get('/api/files/{file_id}', \Telegram\Handler\FileProxyHandler::class);
-
-    $app->post('/api/webhook/amo/{scope_id}', [AmoJoWebhookMiddleware::class, AmoJoWebhookHandler::class]);
-    $app->post('/api/webhook/telegram', [TelegramWebhookMiddleware::class, TelegramWebhookHandler::class]);
-    $app->post('/api/webhook/token-telegram', [
-        SettingsIntegrationMiddleware::class,
-        SettingsIntegrationHandler::class
+    $app->post('/api/telegram/webhook', [
+        \Telegram\Middleware\TelegramWebhookMiddleware::class,
+        \Telegram\Handler\TelegramWebhookHandler::class
     ]);
 };
